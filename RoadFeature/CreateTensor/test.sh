1e45c4a223e041d8db4be61adfeee26df55dd5c7
lynnjyl <lynnjyl@gmail.com>
2015-11-03 17:13:15 +0800

    update

--- RoadFeature/CreateTensor/test.sh
+++ RoadFeature/CreateTensor/test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-trajid=9301;
+trajid=1;
 before="";
-:<<BLOCK
+
 for ((k=0;k<10;k++))
 do
     path="../modified/00${k}"
@@ -13,10 +13,7 @@ do
 
     cd ../../CreateTensor
     for kfile in $filelist
-    do
-     #   echo $path
-     #   echo $kfile
-	    
+    do	    
 	name=${kfile:0:14}
 	if [ "$name" != "$before" ];then
 		echo $name
@@ -25,12 +22,11 @@ do
 		file1=$path"/"$name".plt_edge.txt"
 		file2=$path"/"$name".plt_traj.txt"
 		echo $file1
-		./main $trajid $file1 $file2 30
+		./main $trajid $file1 $file2 60
 		trajid=$((trajid+1))
 	else
 		echo "the same"
 	fi
-#    ./main ./roadnetwork/grid_edge_0.05.txt $path"/"$kfile
     done
 done
 
@@ -45,9 +41,6 @@ do
     cd ../../CreateTensor
     for kfile in $filelist
     do
-     #   echo $path
-     #   echo $kfile
-	    
 	name=${kfile:0:14}
 	if [ "$name" != "$before" ];then
 		echo $name
@@ -56,15 +49,14 @@ do
 		file1=$path"/"$name".plt_edge.txt"
 		file2=$path"/"$name".plt_traj.txt"
 		echo $file1
-		./main $trajid $file1 $file2 30
+		./main $trajid $file1 $file2 60
 		trajid=$((trajid+1))
 	else
 		echo "the same"
 	fi
-#    ./main ./roadnetwork/grid_edge_0.05.txt $path"/"$kfile
     done
 done
-BLOCK
+
 for ((k=100;k<182;k++))
 do
     path="../modified/${k}"
@@ -75,10 +67,7 @@ do
 
     cd ../../CreateTensor
     for kfile in $filelist
-    do
-     #   echo $path
-     #   echo $kfile
-	    
+    do	    
 	name=${kfile:0:14}
 	if [ "$name" != "$before" ];then
 		echo $name
@@ -87,62 +76,12 @@ do
 		file1=$path"/"$name".plt_edge.txt"
 		file2=$path"/"$name".plt_traj.txt"
 		echo $file1
-		./main $trajid $file1 $file2 30
+		./main $trajid $file1 $file2 60
 		trajid=$((trajid+1))
 	else
 		echo "the same"
 	fi
-#    ./main ./roadnetwork/grid_edge_0.05.txt $path"/"$kfile
     done
 done
-:<<BLOCK
-for ((k=100;k<101;k++))
-do
-    path="./result/${k}"
-    cd $path
-    filelist=$(ls)
-    cd ../..
-    for kfile in $filelist
-    do
-        echo $path
-        echo $kfile
-        ./main ./roadnetwork/grid_edge_0.05.txt $path"/"$kfile
-    done
 
-done
-
-
-for ((k=101;k<182;k++))
-do
-    path="./result/${k}"
-    cd $path
-    filelist=$(ls)
-    cd ../..
-    for kfile in $filelist
-    do
-        echo $path
-        echo $kfile
-        ./main ./roadnetwork/grid_edge_0.05.txt $path"/"$kfile
-    done
-
-
-done
-BLOCK
-
-
-#path=$1
-#cd $path
-
-#	tmp=`basename $kfile`
-#	if [ $tmp == "test.sh" ];then
-#		cd ${tmp}
-#		for aFile in *
-#		do
-#            filename="~/Codes/GCode/result/${tmp}/${aFile}"
-#            cd ..
-#            ./main ./roadnetwork/grid_edge_0.05.txt $filename
-#		done
-#	fi
-#	cd ..
-done
 
---
+++ dtw/main.cpp
@@ -0,0 +1,119 @@
+/* sscanf example */
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <time.h>
+using namespace std;
+
+//20081023025304.plt
+
+double TRAC1[20000][2];
+double TRAC2[20000][2];
+int nTRAC1, nTRAC2;
+char f1[] = "20081023025304.plt";
+char f2[] = "20090413213935.plt";
+
+double* DP;
+
+double dist(int i,int j){
+	double a = TRAC1[i][0] - TRAC2[j][0];
+	double b = TRAC1[i][1] - TRAC2[j][1];
+	return sqrt(a*a + b*b);
+}
+
+double DWT(){
+	
+	//double* DP =(double*) malloc(sizeof(double)*nTRAC1*nTRAC2);
+	int i, j;
+	double a, b, c;
+	for (j = 0; j < nTRAC2; j++)
+		*(DP + j) = 0;
+	for (i = 0; i < nTRAC1; i++)
+		*(DP + nTRAC2*i) = 0;
+
+	for (i = 1; i < nTRAC1; i++)
+		for (j = 1; j < nTRAC2; j++)
+		{
+			a = *(DP + nTRAC2*(i-1) + j);
+			b = *(DP + nTRAC2*i + j-1);
+			c = *(DP + nTRAC2*(i-1) + j-1);
+			double mm = min(min(a,b),c);
+			*(DP + nTRAC2*i + j) = dist(i, j) + mm;
+			//printf("%lf %lf %lf\n", a, b, c);
+			//system("pause");
+			
+			//printf("%lf\n", *(DP + nTRAC2*i + j));
+			//
+		}
+	double ans = *(DP + nTRAC2*(nTRAC1 - 1) + nTRAC2 - 1);
+	//free(DP);
+	return ans;
+}
+
+int main()
+{
+	nTRAC1 = 0, nTRAC2 = 0;
+
+	FILE * pFile;
+	char mystring[100];
+	double x, y;
+	pFile = fopen(f1, "r");
+	if (pFile == NULL) perror("Error opening file");
+	else {
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		while (fgets(mystring, 100, pFile) != NULL)
+		{
+			//sscanf(mystring, "%s %*s %d", str, &i);
+			sscanf(mystring, "%lf,%lf", &x, &y);
+			//puts(mystring);
+			//printf("%.6lf, %.6lf", x,y);
+			TRAC1[nTRAC1][0] = x;
+			TRAC1[nTRAC1][1] = y;
+			nTRAC1++;
+		}
+		fclose(pFile);
+	}
+
+	pFile = fopen(f2, "r");
+	if (pFile == NULL) perror("Error opening file");
+	else {
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		fgets(mystring, 100, pFile);
+		while (fgets(mystring, 100, pFile) != NULL)
+		{
+			//sscanf(mystring, "%s %*s %d", str, &i);
+			sscanf(mystring, "%lf,%lf", &x, &y);
+			//puts(mystring);
+			//printf("%.6lf, %.6lf", x, y);
+			TRAC2[nTRAC2][0] = x;
+			TRAC2[nTRAC2][1] = y;
+			nTRAC2++;
+		}
+		fclose(pFile);
+	}
+
+
+	DP = (double*)malloc(sizeof(double)*nTRAC1*nTRAC2);
+	clock_t t;
+	t = clock();
+
+	printf("%.6lf\n",DWT());
+
+	t = clock() - t;
+	printf("It took me %f seconds.\n", ((float)t) / CLOCKS_PER_SEC);
+
+	free(DP);
+	//system("pause");
+
+	return 0;
+}
