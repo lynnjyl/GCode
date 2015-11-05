e4d209800785cae254c82a3edcfe19b319b187c7
lynnjyl <lynnjyl@gmail.com>
2015-11-05 18:42:27 +0800

    update

---
+++ GridMapping/Point.cpp
@@ -0,0 +1,189 @@
+//
+//  Points.cpp
+//
+//
+//  Created by XoX on 15/9/28.
+//
+//
+
+#include "Point.h"
+#include "stdafx.h"
+#include "basis.h"
+
+///////////////////Point Class Begin//////////////////////
+
+//get the radius of a degree
+double Point::getRad(double deg)
+{
+    return PI/180.0*deg;
+}
+
+//get the degree of a point's radius
+double Point::getDeg(double rad)
+{
+    double tmp = 180/PI*rad;
+    if(tmp < 0)
+        tmp = 180 + tmp;
+    return tmp;
+}
+
+//construct
+Point::Point():x(0),y(0),z(0){}
+
+/*
+Point::Point(const GeoPoint& p)
+{
+    double lon = getRad(p.longitude);
+    double lat = getRad(p.latitude);
+    x = cos(lat)*cos(lon);
+    y = cos(lat)*sin(lon);
+    z = sin(lat);
+}
+
+*/
+/*
+Point::Point(double _x, double _y, int _id):id(_id)
+{
+    double lon = getRad(_x);
+    double lat = getRad(_y);
+    x = cos(lat)*cos(lon);
+    y = cos(lat)*sin(lon);
+    z = sin(lat);
+}
+*/
+Point::Point(double _lat,double _lon)
+{
+    if(_lat == 0 && _lon == 0)
+    {
+        gid = 0;
+        _lon = 0;
+        _lat = 0;
+        x = y = z =0;
+    }
+    else
+    {
+        //gid = GetGID(_lat,_lon);
+        _lon = getRad(_lon);
+        _lat = getRad(_lat);
+        //printf("gid : %i\n", gid);
+        x = cos(_lat)*cos(_lon);
+        y = cos(_lat)*sin(_lon);
+        z = sin(_lat);
+    }
+}
+
+
+Point::Point(const double& _x,const double& _y,const double& _z):x(_x),y(_y),z(_z)
+{
+
+}
+
+//return the eulude distance between two points
+double Point::EucDisTo(const Point& p)
+{
+    return EARTHRADIUS*sqrt(SQR(x-p.x)+SQR(y-p.y)+SQR(z-p.z));
+}
+
+//return the geometry distance between two points
+double Point::GeoDisTo(Point& p)
+{
+    //return EARTHRADIUS*sqrt(SQR(x-p.x)+SQR(y-p.y)+SQR(z-p.z));
+    double cosTheta = degBetween(p);
+    return EARTHRADIUS*sqrt(fabs(1.0-SQR(cosTheta)));
+}
+
+//return the degree between two points
+double Point::degBetween(Point& p)
+{
+    return (*this * p) / (length()*p.length());
+}
+
+Point Point::operator + (const Point& p)
+{
+    return Point(x+p.x,y+p.y,z+p.z);
+}
+
+Point Point::operator - (const Point& p)
+{
+    return Point(x-p.x,y-p.y,z-p.z);
+}
+
+double Point::operator * (const Point& p)
+{
+    return x*p.x + y*p.y + z*p.z;
+}
+
+Point Point::operator * (const double& ratio)
+{
+    return Point(x*ratio,y*ratio,z*ratio);
+}
+
+Point Point::operator / (const double& ratio)
+{
+    if(ratio == 0)
+    {
+        fprintf(stderr,"vector devided by zero!\n");
+//        system("pause");
+//        exit(-1);
+    }
+    return Point(this->x/ratio,this->y/ratio,this->z/ratio);
+}
+double Point::length()
+{
+    return sqrt(SQR(x)+SQR(y)+SQR(z));
+}
+
+double Point::getLat()
+{
+    //assert(z >=-1 && z <= 1);
+    return getDeg(asin(z));
+}
+
+double Point::getLon()
+{
+    //assert(fabs(x) > eps);
+    return getDeg(atan(y/x));
+}
+///////////////////Point Class End//////////////////////
+
+
+Point pToseg(Point p,Point begin,Point end){
+    Point retVal;
+
+    double dx = begin.x - end.x;
+    double dy = begin.y - end.y;
+    double dz = begin.z - end.z;
+    if(fabs(dx) < eps && fabs(dy) < eps && fabs(dz) < eps )
+    {
+        //printf("one\n");
+        retVal = begin;
+        return retVal;
+    }
+
+    double v1 = ((p-begin)*(end-begin));
+    double v2 = ((p-end)*(begin-end));
+    if(  v1*v2  < 0)
+    {
+        //printf("two\n");
+        return p.EucDisTo(begin) < p.EucDisTo(end)?begin:end;
+    }
+
+    double u = (p.x - begin.x)*(begin.x - end.x)
+    + (p.y - begin.y)*(begin.y - end.y)
+    + (p.z - begin.z)*(begin.z - end.z);
+    u = u/((dx*dx)+(dy*dy)+(dz*dz));
+
+    retVal.x = begin.x + u*dx;
+    retVal.y = begin.y + u*dy;
+    retVal.z = begin.z + u*dz;
+    //printf("three\n");
+    return retVal;
+}
+
+double dispToseg(Point p,Point a,Point b)
+{
+    //printf("disptoseg\n");
+    Point tmp = pToseg(p,a,b);
+    //DisplayAPoint(tmp);
+    return p.EucDisTo(tmp);
+}
---
+++ GridMapping/main.cpp
@@ -0,0 +1,236 @@
+/*
+        Get all stay points in the dataset
+	input parameter:
+		[1] Number	: file name in ../Geolife Trajectories 1.3/Data/
+		[2]	D		: distance threshold
+		[3] T		: time threshold
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <sstream>
+
+using namespace std;
+
+#define pi 3.14159265358979323846
+
+struct GPSlog
+{
+	double Latitude;
+	double Longitude;
+	string Time;
+};
+
+struct StayPoints
+{
+	double Lat;
+	double Lngt;
+	double arvT;
+	double levT;
+};
+
+double deg2rad(double deg)
+{
+	return (deg * pi / 180);
+}
+
+double rad2deg(double rad)
+{
+	return (rad * 180 / pi);
+}
+
+double GetTime(string str)
+{
+    double time;
+    int p1, p2;
+    int hour, minute, second;
+    p1 = str.find_first_of(':', 0);
+    p2 = str.find_last_of(':');
+   // cout << "p1 = " << p1 << " p2 = " << p2 << endl;
+    hour = atoi(str.substr(0, p1).c_str());
+    p1++;
+    minute = atoi(str.substr(p1, p2-p1).c_str());
+    p2++;
+    second = atoi(str.substr(p2, str.length()-p2).c_str());
+    //cout << hour << ":" << minute << ":" << second << endl;
+    //cout << str << endl;
+    time = hour*60*60+minute*60+second;
+    return time;
+}
+
+double timebetween(GPSlog &l1, GPSlog &l2)
+{
+    //cout << "in time between func" << endl;
+	//return (l2.Time - l1.Time)*24*3600;
+    double t1, t2;
+    double time;
+    t1 = GetTime(l1.Time);
+    t2 = GetTime(l2.Time);
+    time = t2-t1;
+    if(time < 0)
+        time = time + 24*60*60;
+    //cout << "time between: " << time << endl;
+    return time;
+}
+
+double distance(double &lat1, double &lon1, double &lat2, double &lon2)
+// double distance(GPSlog log1, GPSlog log2)
+{
+	double theta, dist;
+
+	theta = lon1 - lon2;
+	dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta));
+
+	dist = acos(dist);
+	dist = rad2deg(dist);
+	dist = dist * 60 * 1.1515;
+	dist = dist * 1609.344;
+	return (dist);
+}
+
+string getdate(string s)
+{
+	int pos1, pos2;
+	pos1 = pos2 = 0;
+	for(int i = 0; i < 4; i++)
+	{
+		pos1 = s.find_first_of(',', pos1);
+		pos1++;
+	}
+	pos2 = s.find_first_of('.', pos1);
+	// printf("%d, %d\n", pos1, pos2);
+	return s.substr(pos1, pos2 - pos1);
+}
+
+void ReadTraj(string input, vector <GPSlog> & traj)
+{
+    FILE *fp = fopen(input.c_str(), "r");
+
+    double lat, lng;
+    char time[20];
+    GPSlog point;
+    string t;
+    while(fscanf(fp, "%lf %lf %s\n", &lat, &lng, time) != EOF)
+    {
+        point.Latitude = lat;
+        point.Longitude = lng;
+        t = string(time);
+        point.Time = t;
+        traj.push_back(point);
+    }
+    return;
+}
+
+void DetectRegion(double D, double T, double Time, vector <GPSlog> & traj, vector <int> & StartIndex, vector <int> &EndIndex )
+{
+    StartIndex.push_back(0);
+    int i = 0;
+    int m, start;
+    int size = traj.size();
+    double Time_sec = Time*60;
+    double TimeGap;
+
+    for(i = 0; i < size-1; i++)
+    {
+        TimeGap = timebetween(traj[i], traj[i+1]);
+        if(TimeGap > Time_sec)
+        {
+            StartIndex.push_back(i+1);
+            EndIndex.push_back(i);
+        }
+    }
+
+    cout << "detect" << endl;
+    start = 1;
+	m = 0;
+    while(m < size)
+	{
+		/*find the index startindex that satisfy the time threshold*/
+		//cout << m << endl;
+		while(start < size && (timebetween(traj[m], traj[start]) < T*60) )
+		{
+			start++;
+		}
+		//cout << "a time "  <<m <<" " << start<< endl;
+		/*check if all the points satisfy the distance threshold*/
+		i = start-1;
+		while((distance(traj[i].Latitude, traj[i].Longitude, traj[m].Latitude, traj[m].Longitude) <= D) && i > m )
+			i--;
+
+		if(i == m + 1 && (timebetween(traj[m], traj[start]) >= T*60))
+		{
+            cout << "detect a region" << endl;
+			/*if all the points satisfy the distance,increase startindex to find the first index that un-satisfy*/
+			start++;
+			while(start < size &&(distance(traj[start].Latitude, traj[start].Longitude, traj[m].Latitude, traj[m].Longitude) <= D))
+				start++;
+			//start--;
+
+            cout << "start: " << m << endl;
+            cout << "end: " << start << endl;
+			EndIndex.push_back(m);
+			StartIndex.push_back(start);
+
+			m = start;
+			start = m+1;
+		}
+		else
+		{
+			m++;
+		}
+		//cout << "ok " << endl;
+	}
+    cout << "here" << endl;
+	EndIndex.push_back(size-1);
+}
+
+int main(int argc, char * argv[])
+{
+    string input = argv[1];
+    double D = atof(argv[2]);
+    double T = atof(argv[3]);
+    double Time = atof(argv[4]);
+    vector <int> StartIndex;
+    vector <int> EndIndex;
+    vector <GPSlog> traj;
+    int parts;
+    string output, filename;
+    int i;
+    char a = '1';
+    int fstart, fend;
+
+    ReadTraj(input, traj);
+    DetectRegion(D, T, Time, traj, StartIndex, EndIndex);
+
+    cout << "begin to write" << endl;
+    parts = StartIndex.size();
+    cout << "There are(is) " << parts << " part(s) in this trajectory" << endl;
+
+    fstart = input.find_last_of('/');
+    fend = input.find_last_of('.');
+    //cout << fstart << " " << fend << endl;
+    filename = input.substr(fstart, fend-fstart);
+    cout << filename << endl;
+    for(i = 0; i < parts; i++)
+    {
+        output = "./trajectory" + filename +"_" + a + ".txt";
+        cout << output<< endl;
+        FILE *fp = fopen(output.c_str(), "w");
+
+        for(int j = StartIndex[i]; j < EndIndex[i]; j++)
+        {
+            fprintf(fp, "%lf %lf %s\n", traj[j].Latitude, traj[j].Longitude, traj[j].Time.c_str());
+
+        }
+        a++;
+    }
+
+
+
+}
