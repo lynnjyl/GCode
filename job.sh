fb6b5467446c4cef4e6266846518686d4851824f
yiling <lynnjyl@gmail.com>
2015-11-30 09:41:54 +0800

    add results

--- job.sh
+++ job.sh
@@ -3,24 +3,104 @@
 for ((i=0;i<50;i++))
 do
 	cd ./LSH
-	./m600 1000 15822
+	./m1200 1000 15822
 	cd ..
-	./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 600.txt
+	./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 1200.txt
 done
 
 for ((i=0;i<50;i++))
 do
         cd ./LSH
-        ./m800 1000 15822
+        ./m1400 1000 15822
         cd ..
-        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt  >> 800.txt
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt  >> 1400.txt
 done
 
 
 for ((i=0;i<50;i++))
 do
         cd ./LSH
-        ./m1000 1000 15822
+        ./m1600 1000 15822
         cd ..
-        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 1000.txt
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 1600.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m1800 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 1800.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m2000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 2000.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m3000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 3000.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m4000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 4000.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m5000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 5000.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m6000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 6000.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m7000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 7000.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m8000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 8000.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m9000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 9000.txt
+done
+
+for ((i=0;i<50;i++))
+do
+        cd ./LSH
+        ./m10000 1000 15822
+        cd ..
+        ./dtw/eval 300 ./dtw/RateResult/candidate_15320100127234004_sort.plt ./LSH/RbsLsh/cant_15822.txt >> 10000.txt
 done
