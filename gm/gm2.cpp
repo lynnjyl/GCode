60fda2832b3205d89e2f0138db40617db1b3aaad
lynnjyl <lynnjyl@gmail.com>
2016-02-25 13:49:51 +0800

    update

--- gm/gm2.cpp
+++ gm/gm2.cpp
@@ -10,6 +10,8 @@
 #include <math.h>
 #include <functional>
 #include <stdlib.h>
+#include <stdio.h>
+#include <vector>
 
 using namespace std;
 
@@ -20,12 +22,14 @@ struct GPSpoint
 	double lng;
 };
 
+
+
 // six circle of Beijing
 double minlat = 39.688;
 double minlng = 116.093;
 double maxlat = 40.178;
 double maxlng = 116.708;
-double differ = 0.0001;
+double differ = 0.0005;
 int lat_num = (maxlat - minlat)/differ + 1;
 int lng_num = (maxlng - minlng)/differ + 1;
 int size = int(lat_num * lng_num) + 1;
@@ -54,7 +58,6 @@ double rad2deg(double rad)
 	return (rad * 180 / pi);
 }
 double distance(double &lat1, double &lon1, double &lat2, double &lon2)
-// double distance(GPSlog log1, GPSlog log2)
 {
 	double theta, dist;
 
@@ -69,6 +72,20 @@ double distance(double &lat1, double &lon1, double &lat2, double &lon2)
 }
 
 
+double GetAngle(double lat1, double lng1, double lat2, double lng2)
+{
+	//cout << lat1 << " " << lng1 << " " << lat2 << " " << lng2 << endl;
+	double angle;
+	double cosine = (lng1 - lng2)/(sqrt((lng1 - lng2)*(lng1 - lng2) + (lat1 - lat2) * (lat1 - lat2)));
+	angle = acos (cosine) * 180 / pi;
+
+	if(lat1 - lat2 < 0)	
+		angle = -angle;
+
+	return angle;
+}
+
+
 int main(int argc, char * argv[])
 {
 	GPSpoint nw, ne, sw, se;
@@ -78,25 +95,28 @@ int main(int argc, char * argv[])
 	double lat, lng, lat_differ, lng_differ, differ_temp, cornerdiffer;
 	char time[20];
 	int lat_id, lng_id, grid_id;
-	int grid_temp, i;
-	//double dist, value;
+	int i;
 	double dist, value[4], sum;
-	GPSpoint centers[4];			// the 9 related grids.
+	GPSpoint centers[4], temp_point;			// the 4 related grids.
 	int centersid[4];
-	//string filename = "../result/00020081023025304.plt";
-	bool flag1, flag2;
-	flag1 = flag2 = false;
+	vector <GPSpoint> grid_point;
+	int old_grid = 0;
+	vector < vector <double> > grid_angle;
+	grid_angle.resize(size);
+	//GridAngle angle;
+	double angle;
+	
+	/* open GPS file and read it */
 	int trajid = atoi(argv[1]);
 	string filename = argv[2];
 	FILE * fp = fopen(filename.c_str(), "r");
 	cout << trajid << " " << filename <<  endl;
+	
 	cornerdiffer = sqrt(differ*differ*2);
-
-
-	//cout << lat_num << " " << lng_num << endl;
 	int counter = 1;
-	//int a;
-	//cout << GridWeight[1] << endl;
+	//cout << lng_num << endl;
+
+	/* read file : each GPS point*/
 	while(fscanf(fp, "%lf %lf %s\n", &lat, &lng, time) != EOF)
 	{
 		//fscanf(fp, "%lf %lf %s\n", &lat, &lng, time);
@@ -105,15 +125,50 @@ int main(int argc, char * argv[])
 		lng_id = ceil((lng - minlng)/differ);
 		grid_id = lat_id*lng_num + lng_id;		//Id of the grid which the GPS point is in.
 
-		//cout << lat << " " << lng << endl;
-		//cout << lat_id << " " << lng_id << " " << grid_id << endl;
-
-
 		centers[0].lat =  minlat + differ * (lat_id - 1) + differ/2;
 		centers[0].lng = minlng + differ * (lng_id -1) + differ/2;
 		centersid[0] = grid_id;
 
-		//cout << "centers[0]: " << centersid[0] << " " << centers[0].lat << " " << centers[0].lng << endl;
+		
+		/*angle part*/
+		if(old_grid == 0)
+			old_grid = grid_id;
+		temp_point.lat = lat;
+		temp_point.lng = lng;
+		//cout << "grid id = " << grid_id << endl;
+
+		if(old_grid == grid_id)
+		{
+			grid_point.push_back(temp_point);
+		}
+		else
+		{
+		//	cout << old_grid << " grid_point size " << grid_point.size() << endl;
+			if(grid_point.size() != 0)
+			{
+				if(grid_point.size() == 1)
+					angle = GetAngle(grid_point[0].lat, grid_point[0].lng, lat, lng);
+				else
+					angle = GetAngle(grid_point[0].lat, grid_point[0].lng, grid_point[grid_point.size()-1].lat, grid_point[grid_point.size()-1].lng);
+			
+			grid_angle[old_grid].push_back(angle);
+			grid_angle[old_grid+1].push_back(angle);
+			grid_angle[old_grid-1].push_back(angle);
+			grid_angle[old_grid+lng_num].push_back(angle);
+			grid_angle[old_grid+lng_num+1].push_back(angle);
+			grid_angle[old_grid+lng_num-1].push_back(angle);
+			grid_angle[old_grid-lng_num].push_back(angle);
+			grid_angle[old_grid-lng_num+1].push_back(angle);
+			grid_angle[old_grid-lng_num-1].push_back(angle);
+
+			
+			}
+			old_grid = grid_id;
+			grid_point.erase(grid_point.begin(), grid_point.end());
+			grid_point.push_back(temp_point);
+		}
+		/**************************/
+
 
 		centers[1].lng = centers[0].lng;
 		if(lat > centers[0].lat)
@@ -126,7 +181,6 @@ int main(int argc, char * argv[])
 			centers[1].lat = centers[0].lat - differ;
 			centersid[1] = grid_id - lng_num;
 		}
-		//cout << "centers[1]: " << centersid[1] << " " << centers[1].lat << " " << centers[1].lng << endl;
 
 		centers[2].lat = centers[0].lat;
 		if(lng > centers[0].lng)
@@ -139,12 +193,10 @@ int main(int argc, char * argv[])
 			centers[2].lng = centers[0].lng - differ;
 			centersid[2] = grid_id -1;
 		}
-		//cout << "centers[2]: " << centersid[2] << " " << centers[2].lat << " " << centers[2].lng << endl;
-
 
 		centers[3].lat = centers[1].lat;
 		centers[3].lng = centers[2].lng;
-		//centersid[3] = floor((centers[3].lat - minlat)/differ)*lng_num + ceil((centers[3].lng - minlng)/differ);
+		
 		if(centersid[2] > centersid[0])
 			centersid[3] = centersid[1] + 1;
 		else
@@ -168,51 +220,24 @@ int main(int argc, char * argv[])
 			}
 		}
 
-
-		//cin >> a;
-
-		/*//calculate the 9 grids related to the GPS point.
-		//the center, index = 4;
-		centers[4].lat = minlat + differ * (lat_id - 1) + differ/2;
-		centers[4].lng = minlng + differ * (lng_id -1) + differ/2;
-
-		centers[0].lat = centers[1].lat = centers[2].lat = centers[4].lat + differ;
-		centers[3].lat = centers[5].lat = centers[4].lat;
-		centers[6].lat = centers[7].lat = centers[8].lat = centers[4].lat - differ;
-		
-		centers[0].lng = centers[3].lng = centers[6].lng = centers[4].lng - differ;
-		centers[1].lng = centers[7].lng = centers[4].lng;		
-		centers[2].lng = centers[5].lng = centers[8].lng = centers[4].lng + differ;
-
-		//get the ids of the grids related.
-		centersid[4] = grid_id;
-
-		centersid[1] = grid_id + lng_num;
-		centersid[7] = grid_id - lng_num;
-		centersid[3] = grid_id -1;
-		centersid[5] = grid_id +1;
-
-		centersid[0] = centersid[1] - 1;
-		centersid[2] = centersid[1] + 1;
-		centersid[6] = centersid[7] - 1;
-		centersid[8] = centersid[7] + 1;		
-		
-		//get the weight value based on the distance;
-		for(i = 0; i < 9; i++)
-		{
-			dist = distance(centers[i].lat, centers[i].lng, lat, lng);
-			value = 1/dist;
-			if(centersid[i] > 0 && centersid[i] < size)
-				GridWeight[centersid[i]] = max(value, GridWeight[centersid[i]]);
-		}*/
 	}
 	fclose(fp);
 
-	fp = fopen("matrix_133.txt", "a");
+	//cout << "here" << endl;
+
+	fp = fopen("matrix_225.txt", "a");
+	FILE *anglefile = fopen("angle_225.txt", "a");
 	for(int i = 0; i < size; i++)
 	{
 		if(GridWeight[i] != 0)
 			fprintf(fp, "%d %d %lf\n", trajid, i, GridWeight[i]);
+		if(grid_angle[i].size()!=0)
+		{
+			fprintf(anglefile, "%d %d", trajid, i);
+			for(int j = 0; j < grid_angle[i].size(); j++)
+				fprintf(anglefile, " %lf", grid_angle[i][j]);
+			fprintf(anglefile, "\n");
+		}
 	}
 
 	return 0;
