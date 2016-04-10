79d34274c879d73cc870a684f1d865fde7765fd2
lynnjyl <lynnjyl@gmail.com>
2016-04-10 15:17:41 +0800

    update

--- full/lcss.cpp
+++ full/lcss.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <math.h>
 #include <sys/time.h>
+#include <ctime>
 using namespace std;
 
 struct GPSPoint
@@ -59,9 +60,13 @@ int main(int argc, char *argv[])
 
 //	fp1 = fopen(argv[1], "r");
 //	fp2 = fopen(argv[2], "r");
-	int tid = atoi(argv[1]);
-	int delta = atoi(argv[2]);
-	double epsilon = atof(argv[3]);
+	
+	srand((unsigned)time(0));
+	//int tid = atoi(argv[1]);
+	int tid;
+	int delta = atoi(argv[1]);
+	double epsilon = atof(argv[2]);
+	int count = atoi(argv[3]);
 	double latitude, longitude;
 	char temp[20];
 	double dist;
@@ -69,64 +74,72 @@ int main(int argc, char *argv[])
 	GPSPoint point;
 	int m, n, i(0), j(0);
 	int *lcs;
-
+	
 	string queryfile, cantfile;
-
-	queryfile = "../Data/new/" + to_string(tid);
-	fp1 = fopen(queryfile.c_str(), "r");
-	while(fscanf(fp1, "%lf %lf %s\n", &point.lat, &point.lng, temp) != EOF)
-	{
-		query.push_back(point);
-		i++;
-	}
-	m = i;
-	fclose(fp1);
 
 	double t1, t2, tsum(0);
 
-
-	for(int k = 1; k <= 58181; k++)
+	int num = count;
+	while(count > 0)
 	{
-		cantfile = "../Data/new/" + to_string(k);
-		fp2 = fopen(cantfile.c_str(), "r");
-		while(fscanf(fp2, "%lf %lf %s\n", &point.lat, &point.lng, temp) != EOF)
+		tid = rand()%58181 + 1;
+		queryfile = "../Data/new/" + to_string(tid);
+		fp1 = fopen(queryfile.c_str(), "r");
+		while(fscanf(fp1, "%lf %lf %s\n", &point.lat, &point.lng, temp) != EOF)
 		{
-			cant.push_back(point);
-			j++;
+			query.push_back(point);
+			i++;
 		}
-		fclose(fp2);
-		n = j;
-		t1 = wallclock();
-		lcs = new int [(m+1)*(n+1)];
+		m = i;
+		fclose(fp1);
 
-		for(i = 1; i <= m; i++)
-			lcs[i*(n+1)] = 0;
-		for(j = 0; j <= n; j++)
-			lcs[j] = 0;
+		
 
 
-		for(i = 1; i <= m; i++)
+		for(int k = 1; k <= 58181; k++)
 		{
-			for(j = 1; j <= n; j++)
+			cantfile = "../Data/new/" + to_string(k);
+			fp2 = fopen(cantfile.c_str(), "r");
+			while(fscanf(fp2, "%lf %lf %s\n", &point.lat, &point.lng, temp) != EOF)
 			{
-				if(abs(i-j) <= delta)
+				cant.push_back(point);
+				j++;
+			}
+			fclose(fp2);
+			n = j;
+			t1 = wallclock();
+			lcs = new int [(m+1)*(n+1)];
+
+			for(i = 1; i <= m; i++)
+				lcs[i*(n+1)] = 0;
+			for(j = 0; j <= n; j++)
+				lcs[j] = 0;
+
+
+			for(i = 1; i <= m; i++)
+			{
+				for(j = 1; j <= n; j++)
 				{
-					dist = distance(query[i].lat, query[i].lng, cant[j].lat, cant[j].lng);
-					if(dist <= epsilon)
-						lcs[i*(n+1)+j] = lcs[(i-1)*(n+1) + (j-1)] + 1;
-					else
-						lcs[i*(n+1)+j] = max(lcs[(i-1)*(n+1) + j], lcs[i*(n+1) + j - 1]);
+					if(abs(i-j) <= delta)
+					{
+						dist = distance(query[i].lat, query[i].lng, cant[j].lat, cant[j].lng);
+						if(dist <= epsilon)
+							lcs[i*(n+1)+j] = lcs[(i-1)*(n+1) + (j-1)] + 1;
+						else
+							lcs[i*(n+1)+j] = max(lcs[(i-1)*(n+1) + j], lcs[i*(n+1) + j - 1]);
+					}
 				}
 			}
-		}
 
-		t2 = wallclock();
-		tsum += t2 - t1;
-		j = 0;
-		delete []lcs;
+			t2 = wallclock();
+			tsum += t2 - t1;
+			j = 0;
+			delete []lcs;
+		}
+		count--;
 	}
 
-	cout << "lcss query time is :" << tsum << endl;
+	cout << "lcss query time is :" << tsum/(double)num << endl;
 
 	return 0;
 }
